47e56ecce80f894f86092bb6ad12508128e1485e
Usuario <Usuario@Usuario-PC>
2018-03-11 23:22:48 +0100

    dividir

---
+++ Dividir/src/es/studium/Dividir/Dividir.java
@@ -0,0 +1,30 @@
+package es.studium.Dividir;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Dividir  //He editado este codigo
+
+{
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader lectura = new BufferedReader(new InputStreamReader(System.in));
+		int num1,num2;
+		float division;
+		System.out.println("Ingrese número: ");
+		num1 = Integer.parseInt(lectura.readLine());
+		System.out.println("Deme otro número: ");
+		num2 = Integer.parseInt(lectura.readLine());
+		division = dividir(num1,num2);
+		System.out.println("La división es "+division);
+	}
+	public static float dividir(int a, int b)
+	{
+		float resultado;
+		resultado = (float) a /(float) b;
+		return(resultado);
+	}
+
+
+}
